34aa7e60681dce8f40ac6de7372589813274d036
Thinline <thinline@Thinlines-MacBook-Pro.local>
2015-04-11 01:29:50 +0100

    first rev project

---
+++ PostCard/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  PostCard
+//
+//  Created by Thinline on 4/10/15.
+//  Copyright (c) 2015 Thinline. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var messageLabel: UILabel!
+
+    @IBOutlet weak var enterNameTextField: UITextField!
+    
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    
+    @IBOutlet weak var mailButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // this function is called when send mail button is pressed
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        
+        messageLabel.hidden = false
+        messageLabel.text = enterNameTextField.text
+        messageLabel.textColor = UIColor.redColor()
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
+        
+        mailButton.setTitle("Mail Sent", forState:UIControlState.Normal)
+        
+        
+    }
+
+
+}
+
